ce82a65532cc502f90e68e7c2c0a89d1151e84da
nehalmohsen22 <nehalmoh27@gmail.com>
2022-05-23 19:09:27 +0200

    header and nav-bar and footer

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
